251e1e861ca6260c97320001fc657b27a73c2a34
DESKTOP-5QB8OGM\User <ggamjige8888@naver.com>
2020-07-16 12:48:55 +0900

    fan.catholic.ac.kr-frontend-2 1. (수정 1차)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import Join from "./components/views/Join/Join";
 import Login from "./components/views/Login/Login";
 import Logout from "./components/views/Logout/LogoutButton";
+import Home from "./components/views/Home/Home";
 import './App.css';
 
 function App() {
   return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path="/join" component={Join} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/logout" component={Logout} />
-          </Switch>
-        </div>
-      </Router>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/join" component={Join} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/logout" component={Logout} />
+          <Route exact path="/home" component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 export default App;
