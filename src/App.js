a7b062ecadf63093290c52f567c21381d62f786c
DESKTOP-5QB8OGM\User <ggamjige8888@naver.com>
2020-07-17 18:16:24 +0900

    fan.catholic.ac.kr-frontend-2 1. (수정 1차)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Join from "./components/views/Join/Join";
 import Login from "./components/views/Login/Login";
 import Logout from "./components/views/Logout/LogoutButton";
 import Home from "./components/views/Home/Home";
+import RentBookList from "./components/view/library/RentBookList";
+import BookList from "./components/view/library/BookList";
 import './App.css';
 
 const App = () => {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/join" component={Join} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/logout" component={Logout} />
-          <Route exact path="/home" component={Home} />
-        </Switch>
-      </div>
-    </Router>
-  )
-}
+    return (
+        <Router>
+            <div>
+                 <Switch>
+                    <Route exact path="/join" component={Join} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/logout" component={Logout} />
+                    <Route exact path="/home" component={Home} />
+                    <Route path="/library" component={BookList} />
+                    <Route path="/rentbook" component={RentBookList} />
+                 </Switch>
+            </div>
+        </Router>
+        )
+    }
 export default App;
+
