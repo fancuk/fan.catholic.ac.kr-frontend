6edad3f23f9ca75d52bf672e680c143f51cb44fc
dong-geon <wge3142@gmail.com>
2020-07-17 12:50:07 +0900

    prototype of library system

---
+++ src/components/view/libfunc/BookDelete.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+class BookDelete extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+    deleteBook(seqNo) {
+        const url = '/api/library/' + seqNo;
+        fetch(url, {
+            method: 'DELETE'
+        });
+        this.props.stateRefresh();
+    }
+    handleClickOpen = () => {
+        this.setState({
+            open: true
+        });
+    }
+
+    handleClose = () => {
+        this.setState({
+            open: false
+        })
+    }
+    render() {
+        return (
+            <div>
+                <Button variant="contained" color="secondary" onClick={this.handleClickOpen}>삭제</Button>
+                <Dialog open={this.state.open} onClose={this.handleClose}>
+                    <DialogTitle onClose={this.handleClose}>
+                        삭제 경고
+                    </DialogTitle>
+                    <DialogContent>
+                        <Typography gutterBottom>
+                            선택한 도서가 삭제됩니다.
+                        </Typography>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={(e) => {this.deleteBook(this.props.seqNo)}}>삭제</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+};
+
+export default BookDelete;
