fed117d87f9673660267956ea3968a00cb391677
hyo814 <ggamjige8888@naver.com>
2020-07-30 22:36:34 +0900

    fan.catholic.ac.kr-frontend-15 게시판 만들기

---
+++ src/components/views/Notice/Notice.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import NoticeForm from './NoticeForm';
+import NoticeItem from './NoticeItem';
+import {withRouter} from "react-router-dom";
+
+class Notice extends Component {
+
+    state = {
+        maxNo: 3,
+        boards: [
+            {
+                brdno: 1,
+                brdwriter: '임효진',
+                brdtitle: 'FAN 게시판 입니다.',
+                brditem: '안녕하세요 게시판지기 입니다. 텍스트 입력을 시작하도록 하겠습니다.',
+                brddate: new Date()
+            },
+            {
+                brdno: 2,
+                brdwriter: '임혜린',
+                brdtitle: 'FAN 게시판2 입니다.',
+                brditem: '안녕하세요 게시판지기 입니다. 텍스트 입력을 시작하도록 하겠습니다.',
+                brddate: new Date()
+            },
+        ],
+        selectedBoard:{}
+    }
+
+    handleSaveData = (data) => {
+        if (!data.brdno) {            // new : Insert
+            this.setState({
+                maxNo: this.state.maxNo+1,
+                boards: this.state.boards.concat({brdno: this.state.maxNo, brddate: new Date(), ...data }),
+                selectedBoard: {}
+            });
+        } else {                                                        // Update
+            this.setState({
+                boards: this.state.boards.map(row => data.brdno === row.brdno ? {...data }: row),
+                selectedBoard: {}
+            })
+        }
+    }
+
+    handleRemove = (brdno) => {
+        this.setState({
+            boards: this.state.boards.filter(row => row.brdno !== brdno)
+        })
+    }
+
+    handleSelectRow = (row) => {
+        this.setState({selectedBoard:row});
+    }
+
+    render() {
+        const { boards, selectedBoard } = this.state;
+        return (
+            <div>
+
+                <h1>공지사항 - F.A.N </h1>
+                <NoticeForm selectedBoard={selectedBoard} onSaveData={this.handleSaveData}/>
+                <table border="1">
+                    <tbody>
+                    <tr align="center">
+                        <td width="auto">No.</td>
+                        <td width="auto">제목</td>
+                        <td width="auto">작성자</td>
+                        <td width="auto">내용</td>
+                        <td width="auto">날짜</td>
+                    </tr>
+                    {
+                        boards.map(row =>
+                            (<NoticeItem key={row.brdno} row={row} onRemove={this.handleRemove} onSelectRow={this.handleSelectRow} />)
+                        )
+                    }
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default withRouter(Notice);
