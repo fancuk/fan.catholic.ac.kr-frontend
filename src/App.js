61d6327aef5dbf97ba887c0e2d0c6c6e7f25d9a8
shhb1022 <shhb1022@naver.com>
2020-08-02 15:04:50 +0900

    fan.catholic.ac.kr-frontend-14 메인페이지

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/views/Header/Header";
 import Join from "./components/views/Join/Join";
 import Login from "./components/views/Login/Login";
 import Logout from "./components/views/Logout/LogoutButton";
-import Home from "./components/views/Home/Home";
 import RentBookList from "./components/views/library/RentBookList";
 import BookList from "./components/views/library/BookList";
 import CustomerList from "./components/views/CustomerList/CustomerList";
 import CustomerDetail from "./components/views/CustomerDetail/CustomerDetail"
-import './App.css';
+import MainSlider from "./components/views/Home/MainSlider";
 
 const App = () => {
     return (
-        <Router>
-            <div>
-                 <Switch>
-                    <Route path="/join" component={Join} />
-                    <Route path="/login" component={Login} />
-                    <Route path="/logout" component={Logout} />
-                    <Route path="/home" component={Home} />
-                    <Route path="/library" component={BookList} />
-                    <Route path="/rentbook" component={RentBookList} />
-                    <Route exact path="/member" component={ CustomerList}/>
-                    <Route path="/member/detail" component={CustomerDetail}/>
-                 </Switch>
-            </div>
-        </Router>
+        <div>
+            <Header/>
+            <Router>
+                <div>
+                    <Switch>
+                        <Route path="/home" component={MainSlider} />
+                        <Route path="/join" component={Join} />
+                        <Route path="/login" component={Login} />
+                        <Route path="/logout" component={Logout} />
+                        <Route path="/library" component={BookList} />
+                        <Route path="/rentbook" component={RentBookList} />
+                        <Route exact path="/member" component={CustomerList}/>
+                        <Route path="/member/detail" component={CustomerDetail}/>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
         )
     }
 export default App;
