e5f18f6ddd0d14352d3f536e8ce3f837fb5b0a1f
HYO JIN LIM <59094619+hyo814@users.noreply.github.com>
2020-08-03 21:45:40 +0900

    모듈 오류 처리 전까지는 주석 처리 상태
    모듈 오류 처리 전까지는 주석 처리 상태

--- src/components/views/Home/MainSlider.js
+++ src/components/views/Home/MainSlider.js
@@ -1,39 +1,41 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
+// 모듈 오류로 인하여 잠시 전체 주석 처리 하였습니다!
 
-export default class MainSlider extends Component {
-    render() {
-        const settings = {
-            dots: true,
-            infinite: true,
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
-        };
-        return (
-            <div>
-                <h2>슬라이더</h2>
-                <Slider {...settings}>
-                    <div>
-                        <h3>사진1</h3>
-                    </div>
-                    <div>
-                        <h3>사진2</h3>
-                    </div>
-                    <div>
-                        <h3>사진3</h3>
-                    </div>
-                    <div>
-                        <h3>사진4</h3>
-                    </div>
-                    <div>
-                        <h3>사진5</h3>
-                    </div>
-                    <div>
-                        <h3>사진6</h3>
-                    </div>
-                </Slider>
-            </div>
-        );
-    }
-}
+// import React, { Component } from "react";
+// import Slider from "react-slick";
+//
+// export default class MainSlider extends Component {
+//     render() {
+//         const settings = {
+//             dots: true,
+//             infinite: true,
+//             speed: 500,
+//             slidesToShow: 1,
+//             slidesToScroll: 1
+//         };
+//         return (
+//             <div>
+//                 <h2>슬라이더</h2>
+//                 <Slider {...settings}>
+//                     <div>
+//                         <h3>사진1</h3>
+//                     </div>
+//                     <div>
+//                         <h3>사진2</h3>
+//                     </div>
+//                     <div>
+//                         <h3>사진3</h3>
+//                     </div>
+//                     <div>
+//                         <h3>사진4</h3>
+//                     </div>
+//                     <div>
+//                         <h3>사진5</h3>
+//                     </div>
+//                     <div>
+//                         <h3>사진6</h3>
+//                     </div>
+//                 </Slider>
+//             </div>
+//         );
+//     }
+// }
