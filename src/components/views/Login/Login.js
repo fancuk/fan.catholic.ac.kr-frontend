cfcb31ba9f4d13e0b92a20ea49f854da36a38e85
hyo814 <ggamjige8888@naver.com>
2020-08-07 01:49:39 +0900

    fan.catholic.ac.kr-frontend-28 login/logout/join (심화) 로그인 re-new

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,74 +1,106 @@
-import React, { useState } from "react"
+import React, { Component } from 'react';
+import Axios from 'axios';
 import styled from 'styled-components';
-import { Link,Redirect, withRouter } from "react-router-dom"
+import { Link, Redirect, withRouter } from "react-router-dom";
 import logo from '../../logo.png';
-import {BsFillLockFill} from "react-icons/bs";
+import { BsFillLockFill } from "react-icons/bs";
 
-function Login({ authenticated, login, location }) {
-    const [id, setId] = useState("")
-    const [password, setPassword] = useState("")
 
-    const handleClick = () => {
+class Login extends Component {
+    state = {
+        user_id: '',
+        user_pwd: '',
+        errorMessage: ''
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const body = {
+            user_id: this.state.user_id,
+            user_pwd: this.state.user_pwd,
+        }
+
         try {
-            login({ id, password })
-        } catch (e) {
-            alert("Failed to login")
-            setId("")
-            setPassword("")
+            await Axios.post('fan.catholic.ac.kr:5000/login', body, { withCredentials: true })
+            this.props.history.push('/');
+        } catch (catchedError) {
+            const errorMessage = (catchedError.response && catchedError.response.data)
+                ? catchedError.response.data.errorMessage
+                : catchedError.message;
+            this.setState({
+                errorMessage
+            });
         }
     }
-    const Find = () => {
-        alert("팬 운영진에게 전화해주세요")
+
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
-    const { from } = location.state || { from: { pathname: "/" } }
-    if (authenticated) return <Redirect to={from} />
+    render() {
+        return (
+            <Div>
+                <img src={logo}
+                     className="Login-logo"
+                     alt="logo"
+                     width="300px"
+                     height="300px"
+                />
+                <h1><BsFillLockFill />&nbsp;Login</h1>
+                <h5>Free meeting Active studying Nice ending</h5>
+                <form onSubmit={this.handleSubmit}>
+                    <Input
+                        type='text'
+                        name='user_id'
+                        placeholder='아이디'
+                        onInput={this.handleInput}
+                    />
+                    <br/>
+                    <Input
+                        type='password'
+                        name='password'
+                        placeholder='비밀번호'
+                        onInput={this.handleInput}
+                    />
+                    <br/>
+                    <Link to="./register">
+                        <Button>회원가입</Button>
+                    </Link>
+                    <Button type='submit'>로그인</Button>
+                    <div style={{color: 'red'}}>
+                        {this.state.errorMessage}
+                    </div>
+                </form>
 
-    return (
-        <Div>
-            <img src={logo}
-                 className="Login-logo"
-                 alt="logo"
-                 width="300px"
-                 height="300px"
-            />
-            <h1><BsFillLockFill/>&nbsp;Login</h1>
-            <h5>Free meeting Active studying Nice ending</h5>
-            <input
-                value={id}
-                onChange={({ target: { value } }) => setId(value)}
-                type="text"
-                placeholder="id"
-            /><br/>
-            <input
-                value={password}
-                onChange={({ target: { value } }) => setPassword(value)}
-                type="password"
-                placeholder="password"
-            /><br/>
-            <Link to="./join">
-                <Button>Join</Button>
-            </Link>
-            <Button onClick={handleClick}>Login</Button>
-                <Button onClick={Find}>비밀번호/아이디 찾기</Button>
-
-        </Div>
-    )
+                <h6>비밀번호 분실 시, 운영진에게 문의 해주세요! </h6>
+            </Div>
+        );
+    }
 }
-const Div=styled.div`
+
+const Div = styled.div`
 padding:25px;
 display:block;
 text-align:center;
 `;
 
 const Button =styled.button`
-    display:inline-block;
+    display:display;
     border-radius:10px;
     border-color:#0080ff;
     margin:10px;
     padding:5px;
     font-weight:600;
     background-color:#afdaff;
-   `;
+    text-align: center;
+    `;
+
+const Input = styled.input`
+padding:15px;
+margin:30px;
+`;
 
 export default withRouter(Login);
