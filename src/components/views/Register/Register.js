3dca67fafa9331857149d81cf412a64d214b58bc
hyo814 <ggamjige8888@naver.com>
2020-09-01 00:09:03 +0900

    fan.catholic.ac.kr-frontend-43 Register design

--- src/components/views/Register/Register.js
+++ src/components/views/Register/Register.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link, withRouter } from "react-router-dom"
-import { BsFillPersonPlusFill } from "react-icons/bs";
+import {BsFillLockFill, BsFillPersonPlusFill} from "react-icons/bs";
 import { Validator } from './Validator';
 import { Card, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import styled from "styled-components";
@@ -62,92 +62,92 @@ class Register extends Component {
         return (
             <Div>
             <Card body outline color="primary">
-                <h1><BsFillPersonPlusFill/>&nbsp;Register</h1>
-                <h4>Free meeting Active studying Nice ending</h4>
-                <h5>컴퓨터정보공학부 전공학회 F.A.N 회원가입을 환영합니다 !</h5>
-                <Form onSubmit={this.handleSubmit}>
-                    <FormGroup>
-                    <Label for="user_id">아이디</Label>
-                        <Input
-                            text='text'
-                            name="user_id"
-                            placeholder='아이디'
-                            onChange={this.handleInput}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="user_pwd">비밀번호</Label>
-                        <Input
-                            text='password'
-                            name="user_pwd"
-                            placeholder='비밀번호'
-                            onChange={this.handleInput}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                    <Label for="name">이름</Label>
-                        <Input
-                            text='text'
-                            name="name"
-                            placeholder='이름'
-                            onChange={this.handleInput}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                    <Label for="student_id">학번</Label>
-                        <Input
-                            text='text'
-                            name="student_id"
-                            placeholder='학번'
-                            onChange={this.handleInput}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="grade">학년</Label>
-                        <Input
-                            text='text'
-                            name="grade"
-                            placeholder='학년'
-                            onChange={this.handleInput}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="semester">학기</Label>
-                        <Input
-                            text='text'
-                            name="semester"
-                            placeholder='학기'
-                            onChange={this.handleInput}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="phone">번호</Label>
-                        <Input
-                            text='text'
-                            name="phone"
-                            placeholder='휴대폰 번호'
-                            onChange={this.handleInput}
-                        />
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="email">이메일</Label>
-                        <Input
-                            text='text'
-                            name="email"
-                            placeholder='이메일'
-                            onChange={this.handleInput}
-                        />
-                    </FormGroup>
-                    <Link to="./home">
-                        <Button outline color="primary">취소</Button>{' '}
-                    </Link>
+                    <h1><BsFillPersonPlusFill/> REGISTER <BsFillPersonPlusFill/></h1>
+                    <h4>Free meeting Active studying Nice ending</h4>
+                    <h5>컴퓨터정보공학부 전공학회 F.A.N 회원가입을 환영합니다 !</h5>
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormGroup>
+                            <Label for="user_id">아이디</Label>
+                            <Input
+                                text='text'
+                                name="user_id"
+                                placeholder='아이디'
+                                onChange={this.handleInput}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="user_pwd">비밀번호</Label>
+                            <Input
+                                text='password'
+                                name="user_pwd"
+                                placeholder='비밀번호'
+                                onChange={this.handleInput}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="name">이름</Label>
+                            <Input
+                                text='text'
+                                name="name"
+                                placeholder='이름'
+                                onChange={this.handleInput}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="student_id">학번</Label>
+                            <Input
+                                text='text'
+                                name="student_id"
+                                placeholder='학번'
+                                onChange={this.handleInput}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="grade">학년</Label>
+                            <Input
+                                text='text'
+                                name="grade"
+                                placeholder='학년'
+                                onChange={this.handleInput}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="semester">학기</Label>
+                            <Input
+                                text='text'
+                                name="semester"
+                                placeholder='학기'
+                                onChange={this.handleInput}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="phone">번호</Label>
+                            <Input
+                                text='text'
+                                name="phone"
+                                placeholder='휴대폰 번호'
+                                onChange={this.handleInput}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="email">이메일</Label>
+                            <Input
+                                text='text'
+                                name="email"
+                                placeholder='이메일'
+                                onChange={this.handleInput}
+                            />
+                        </FormGroup>
+                        <Link to="./">
+                            <Button outline color="primary">취소</Button>{' '}
+                        </Link>
                         <Button outline color="primary" type='Submit'>
                             회원가입
                         </Button>
-                    <div style={{color: 'red'}}>
-                        {this.state.errorMessage}
-                    </div>
-                </Form>
+                        <div style={{color: 'red'}}>
+                            {this.state.errorMessage}
+                        </div>
+                    </Form>
             </Card>
             </Div>
         );
