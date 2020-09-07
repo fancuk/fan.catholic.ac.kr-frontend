8f7e2a6deca9ba6816240bfbcd7ce51840f7dddc
hyo814 <ggamjige8888@naver.com>
2020-09-08 01:32:48 +0900

    fan.catholic.ac.kr-frontend-43 Edit Design

--- src/components/views/Profile/Edit.js
+++ src/components/views/Profile/Edit.js
@@ -3,16 +3,71 @@ import { Link, withRouter } from "react-router-dom"
 import { AiFillEdit } from "react-icons/ai";
 import { Card, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import styled from "styled-components";
+import axios from "axios";
 
 
 class Edit extends Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            user_id: '',
+            user_pwd: '',
+            name: '',
+            student_id: '',
+            grade: '',
+            semester: '',
+            phone: '',
+            email: ''
+        }
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        let url = 'http://fan.catholic.ac.kr:5000/api/profile/edit';
+        const edit = {
+            user_id: this.state.user_id,
+            user_pwd: this.state.user_pwd,
+            name: this.state.name,
+            student_id: this.state.student_id,
+            grade: this.state.grade,
+            semester: this.state.semester,
+            phone: this.state.phone,
+            email: this.state.email
+        }
+        axios.put(url, edit)
+            .then(response => {
+                console.log('response : ', JSON.stringify(response))
+            })
+            .catch(e => {
+                console.log(e);
+            })
+        document.location.href="./mypage";
+
+        this.setState({
+            user_id: '',
+            user_pwd: '',
+            name: '',
+            student_id: '',
+            grade: '',
+            semester: '',
+            phone: '',
+            email: ''
+        })
+    }
+
+    handleInput = (e) => {
+        let success = {};
+        success[e.target.name] = e.target.value;
+        this.setState(success);
+    }
+
     render() {
         return (
             <Div>
                 <Card body outline color="primary">
                     <h1><AiFillEdit/> My Page - EDIT <AiFillEdit/></h1>
                     <h4>Free meeting Active studying Nice ending</h4>
-                    <Form onSubmit={this.handleSubmit}>
+                    <Form onSubmit={this.handleFormSubmit}>
                         <FormGroup>
                             <Label for="user_id">아이디</Label>
                             <Input
@@ -85,7 +140,7 @@ class Edit extends Component {
                                 onChange={this.handleInput}
                             />
                         </FormGroup>
-                        <Link to="/mypage">
+                        <Link to="./mypage">
                             <Button outline color="primary">취소</Button>{' '}
                         </Link>
                         <Button outline color="primary" type='Submit'>
