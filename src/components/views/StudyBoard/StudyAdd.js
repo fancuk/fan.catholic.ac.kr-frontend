c920f5024afc1c78fe254e7e5a7cde1f01903390
hyo814 <ggamjige8888@naver.com>
2020-09-08 01:33:57 +0900

    fan.catholic.ac.kr-frontend-43 Study Design

--- src/components/views/StudyBoard/StudyAdd.js
+++ src/components/views/StudyBoard/StudyAdd.js
@@ -1,43 +1,92 @@
 import React from 'react';
-import {Button, Form, FormGroup, Label, Input, FormText, Card} from 'reactstrap';
+import {Button, Form, FormGroup, Label, Input, Card} from 'reactstrap';
 import styled from 'styled-components';
 import { Link, withRouter } from "react-router-dom";
 import {RiPencilLine} from "react-icons/ri";
+import axios from 'axios';
 
-const StudyAdd = () => {
-    return (
-        <Div>
-            <Card body outline color="primary">
-            <h1><RiPencilLine/>스터디 게시판 글쓰기<RiPencilLine/></h1>
-            <Label for="exampleText"> <strong> - 팬이랑 공지랑 - </strong> </Label>
-        <Form>
-            <FormGroup>
-                <Label for="exampleText"> 제목 </Label>
-                <Input type="text" name="text" id="exampleText" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleText"> 작성자 </Label>
-                <Input type="text" name="text" id="exampleText" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleText"> 내용 </Label>
-                <Input type="textarea" name="text" id="exampleText" />
-            </FormGroup>
-            <FormGroup>
-                <Label for="exampleFile">파일 추가</Label>
-                <Input type="file" name="file" id="exampleFile" />
-                <FormText color="muted">
-                </FormText>
-            </FormGroup>
-            <Link to="./study">
-            <Button outline color="primary">취소</Button></Link>{' '}
-            <Link>
-                <Button outline color="primary" type='submit'>글쓰기</Button></Link>
-        </Form>
-        </Card>
-        </Div>
+class StudyAdd extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            seqNo:null,
+            board_name: '',
+            title: '',
+            writer: '',
+            content: ''
+        }
+    }
 
-    );
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        let url='http://fan.catholic.ac.kr:5000/api/board/add';
+        const Study = {
+            seqNo:Number(this.state.seqNo),
+            board_name: this.state.board_name,
+            title: this.state.title,
+            writer: this.state.writer,
+            content: this.state.content
+        }
+        axios.post(url, Study)
+            .then(response => {
+                console.log('response : ', JSON.stringify(response))
+                document.location.href="./study";
+            })
+            .catch(e => {
+                console.log(e);
+            })
+        this.setState({
+            seqNo:null,
+            board_name: '',
+            title: '',
+            writer: '',
+            content: ''
+        })
+    }
+
+    handleInput = (e) => {
+        let success = {};
+        success[e.target.name] = e.target.value;
+        this.setState(success);
+    }
+
+    render() {
+        const {Study} = this.props;
+        return (
+            <Div>
+                <Card body outline color="primary">
+                    <h1><RiPencilLine/>스터디 게시판 글쓰기<RiPencilLine/></h1>
+                    <Label for="StudyAdd"> <strong> - 팬이랑 공부랑 - </strong> </Label>
+                    <Form onSubmit={this.handleFormSubmit}>
+                        <FormGroup>
+                            <Label for="seqNo"> 번호 </Label>
+                            <Input type="text" name="seqNo" value={this.state.seqNo} onChange={this.handleInput} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="board_name"> 게시판명 </Label>
+                            <Input type="text" name="board_name" value={this.state.board_name} onChange={this.handleInput} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="title"> 제목 </Label>
+                            <Input type="text" name="title" value={this.state.title} onChange={this.handleInput} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="writer"> 작성자 </Label>
+                            <Input type="text" name="writer" value={this.state.writer} onChange={this.handleInput} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="content"> 내용 </Label>
+                            <Input type="textarea" name="content" value={this.state.content} onChange={this.handleInput} />
+                        </FormGroup>
+                        <Link to="./study">
+                            <Button outline color="primary">취소</Button></Link>{' '}
+                            <Button outline color="primary" type='submit'>글쓰기</Button>
+                    </Form>
+                </Card>
+            </Div>
+
+        );
+    }
 }
 
 const Div = styled.div`
--- src/components/views/StudyBoard/StudyList.js
+++ src/components/views/StudyBoard/StudyList.js
@@ -2,8 +2,35 @@ import React from 'react';
 import {Button, Table} from 'reactstrap';
 import { RiPencilLine } from "react-icons/ri";
 import { Link} from "react-router-dom";
+import axios from "axios";
+import StudyAdd from "./StudyAdd";
 
-const StudyList = () => {
+class StudyList extends React.Component {
+   state = {
+       loading: false,
+       board_name:''
+   };
+   boardList = async () => {
+       await axios.get('http://fan.catholic.ac.kr:5000/api/board/list')
+           .then(({data}) => {
+               this.setState({
+                   loading: true,
+                   board_name: data
+               });
+           })
+           .catch(e => {
+               console.error(e);
+               this.setState({
+                   loading: false
+               });
+           });
+        };
+    componentDidMount() {
+        this.boardList();
+    }
+
+   render() {
+       const {Study} = this.props;
     return (
         <div>
         <h2><RiPencilLine/> 스터디 게시판 <RiPencilLine/></h2>
@@ -12,29 +39,36 @@ const StudyList = () => {
             <thead>
             <tr>
                 <th>번호</th>
+                <th>게시판 명</th>
                 <th>제목</th>
                 <th>작성자</th>
-                <th>날짜</th>
                 <th>수정</th>
                 <th>삭제</th>
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>게시판 글쓴이 입니다.</td>
-                <td>임효진</td>
-                <td>20.08.30</td>
+                {Study &&
+                Study.map((board) => {
+                    return (
+                        <StudyAdd
+                            seqNo={board.seqNo}
+                            board_name={board.board_name}
+                            title={board.title}
+                            writer={board.writer}
+                        />
+
+                    );
+                })}
                 <td><Link to="./studyadd"><Button outline color="primary" >수정</Button></Link></td>
                 <td><Button outline color="danger">삭제</Button></td>
-            </tr>
             </tbody>
         </Table>
             <Link to="./studyadd">
             <Button outline color="primary" type='submit'>글쓰기</Button>
             </Link>
         </div>
-    );
+        );
+    }
 }
 
 export default StudyList;
