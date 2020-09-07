d048a531b92654ba99719c95b8aa3717af222112
hyo814 <ggamjige8888@naver.com>
2020-09-08 01:33:14 +0900

    fan.catholic.ac.kr-frontend-43 Fan Design

--- src/components/views/FanBoard/FanAdd.js
+++ src/components/views/FanBoard/FanAdd.js
@@ -1,46 +1,96 @@
 import React from 'react';
-import {Button, Form, FormGroup, Label, Input, FormText, Card} from 'reactstrap';
+import {Button, Form, FormGroup, Label, Input, Card} from 'reactstrap';
 import styled from 'styled-components';
 import { Link, withRouter } from "react-router-dom";
 import {BsFillBellFill} from "react-icons/bs";
+import axios from 'axios';
 
-const FanAdd = () => {
-    return (
-        <Div>
-            <Card body outline color="primary">
-            <h1><BsFillBellFill/> 자유게시판 <BsFillBellFill/> </h1>
-            <Label for="exampleText"><strong> - 팬이랑 이야기랑 - </strong></Label>
-            <Form>
-                <FormGroup>
-                    <Label for="exampleText">제목</Label>
-                    <Input type="text" name="text" id="exampleText" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleText"> 작성자 </Label>
-                    <Input type="text" name="text" id="exampleText" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleText"> 내용 </Label>
-                    <Input type="textarea" name="text" id="exampleText" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="exampleFile">파일 추가</Label>
-                    <Input type="file" name="file" id="exampleFile" />
-                    <FormText color="muted">
-                    </FormText>
-                </FormGroup>
-                <Link to="./fan">
-                    <Button outline color="primary">취소</Button></Link>{' '}
-                <Link to="./fan">
-                    <Button outline color="primary" type='submit'>글쓰기</Button>
-                </Link>
-            </Form>
+class FanAdd extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            seqNo:null,
+            board_name: '',
+            title: '',
+            writer: '',
+            content: ''
+        }
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        let url='http://fan.catholic.ac.kr:5000/api/board/add';
+        const Fan = {
+            seqNo:Number(this.state.seqNo),
+            board_name: this.state.board_name,
+            title: this.state.title,
+            writer: this.state.writer,
+            content: this.state.content
+        }
+        axios.post(url, Fan)
+            .then(response => {
+                console.log('response : ', JSON.stringify(response))
+                document.location.href="./fan";
+                // 저장이 안되는 현상 발견
+            })
+            .catch(e => {
+                console.log(e);
+            })
+        this.setState({
+            seqNo:null,
+            board_name: '',
+            title: '',
+            writer: '',
+            content: ''
+        })
+    }
+
+    handleInput = (e) => {
+        let success = {};
+        success[e.target.name] = e.target.value;
+        this.setState(success);
+    }
+
+    render() {
+        const {fan} = this.props;
+        return (
+            <Div>
+                <Card body outline color="primary">
+                    <h1><BsFillBellFill/>자유 게시판 글쓰기<BsFillBellFill/></h1>
+                    <Label for="NoticeAdd"> <strong> - 팬이랑 이야기랑 - </strong> </Label>
+                    <Form onSubmit={this.handleFormSubmit}>
+                        <FormGroup>
+                            <Label for="seqNo"> 번호 </Label>
+                            <Input type="text" name="seqNo" value={this.state.seqNo} onChange={this.handleInput} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="board_name"> 게시판명 </Label>
+                            <Input type="text" name="board_name" value={this.state.board_name} onChange={this.handleInput} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="title"> 제목 </Label>
+                            <Input type="text" name="title" value={this.state.title} onChange={this.handleInput} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="writer"> 작성자 </Label>
+                            <Input type="text" name="writer" value={this.state.writer} onChange={this.handleInput} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="content"> 내용 </Label>
+                            <Input type="textarea" name="content" value={this.state.content} onChange={this.handleInput} />
+                        </FormGroup>
+                        <Link to="./notice">
+                            <Button outline color="primary">취소</Button></Link>{' '}
+                        <Button outline color="primary" type='submit'>글쓰기</Button>
+                    </Form>
                 </Card>
-        </Div>
-    );
+            </Div>
+
+        );
+    }
 }
+
 const Div = styled.div`
-padding:50% auto;
 text-align:left;
 width:50%;
 height:100%;
--- src/components/views/FanBoard/FanList.js
+++ src/components/views/FanBoard/FanList.js
@@ -1,40 +1,74 @@
 import React from 'react';
 import {Button, Table} from 'reactstrap';
-import { BsFillBellFill } from "react-icons/bs";
-import {Link} from "react-router-dom";
+import { Link} from "react-router-dom";
+import axios from "axios";
+import FanAdd from "./FanAdd";
+import {BsFillBellFill} from "react-icons/bs";
 
-const FanList = () => {
-    return (
-        <div>
-            <h2><BsFillBellFill/><b>자유 게시판</b><BsFillBellFill/></h2>
-            <h6><strong> - 팬이랑 이야기랑 - </strong></h6>
-        <Table hover>
-            <thead>
-            <tr>
-                <th>번호</th>
-                <th>제목</th>
-                <th>작성자</th>
-                <th>날짜</th>
-                <th>수정</th>
-                <th>삭제</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>게시판 글쓴이 입니다.</td>
-                <td>임효진</td>
-                <td>20.08.30</td>
-                <td><Link to="./fanadd"><Button outline color="primary" >수정</Button></Link></td>
-                <td><Button outline color="danger">삭제</Button></td>
-            </tr>
-            </tbody>
-        </Table>
-            <Link to="./fanadd">
-                <Button outline color="primary" type='submit'>글쓰기</Button>
-            </Link>
-        </div>
-    );
+class NoticeList extends React.Component {
+    state = {
+        loading: false,
+        board_name:''
+    };
+    boardList = async () => {
+        await axios.get('http://fan.catholic.ac.kr:5000/api/board/list')
+            .then(({data}) => {
+                this.setState({
+                    loading: true,
+                    board_name: data
+                });
+            })
+            .catch(e => {
+                console.error(e);
+                this.setState({
+                    loading: false
+                });
+            });
+    };
+    componentDidMount() {
+        this.boardList();
+    }
+
+    render() {
+        const {Fan} = this.props;
+        return (
+            <div>
+                <h2><BsFillBellFill/> 자유 게시판 <BsFillBellFill/></h2>
+                <h6> <strong> - 팬이랑 이야기랑 - </strong> </h6>
+                <Table hover>
+                    <thead>
+                    <tr>
+                        <th>번호</th>
+                        <th>게시판 명</th>
+                        <th>제목</th>
+                        <th>작성자</th>
+                        <th>수정</th>
+                        <th>삭제</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {Fan &&
+                    Fan.map((board) => {
+                        return (
+                            <FanAdd
+                                seqNo={board.seqNo}
+                                board_name={board.board_name}
+                                title={board.title}
+                                writer={board.writer}
+                            />
+
+                        );
+                    })}
+                    <td><Link to="./fanadd"><Button outline color="primary" >수정</Button></Link></td>
+                    <td><Button outline color="danger">삭제</Button></td>
+                    </tbody>
+                </Table>
+                <Link to="./fanadd">
+                    <Button outline color="primary" type='submit'>글쓰기</Button>
+                </Link>
+            </div>
+        );
+    }
 }
 
-export default FanList;
+export default NoticeList;
