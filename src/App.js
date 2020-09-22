e81c4d3130f567efb046b9403a09cf31e3b30241
hyo814 <ggamjige8888@naver.com>
2020-09-22 22:45:50 +0900

    react-cookies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CookiesProvider, withCookies} from 'react-cookie'
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import MainSlide from "./components/views/Home/MainSlide";
-import MyPage from "./components/views/Profile/MyPage";
-import Edit from "./components/views/Profile/Edit";
-import Register from "./components/views/Login/Register";
 import Login from "./components/views/Login/Login";
-import LoginControls from "./components/views/Login/LoginControls";
-import LogoutButton from "./components/views/Login/LogoutButton";
+import Logout from "./components/views/Login/Logout";
+import Register from "./components/views/Login/Register";
+import MyPage from "./components/views/Profile/MyPage";
 import RentBookList from "./components/views/library/RentBookList";
 import BookList from "./components/views/library/BookList";
 import CustomerDetail from "./components/views/CustomerDetail/CustomerDetail";
@@ -16,35 +15,33 @@ import FanAdd from "./components/views/Free/FanAdd";
 import FanList from "./components/views/Free/FanList";
 import StudyAdd from "./components/views/Study/StudyAdd";
 import StudyList from "./components/views/Study/StudyList";
+import LoginControl from "./components/views/Login/LoginControl";
 import './App.css';
 
-const App = () => {
-    return (
-        <>
-            <Router>
-                <div>
-                    <Switch>
-                        <Route path="/mypage" component={MyPage}/>
-                        <Route path="/edit" component={Edit}/>
-                        <Route exact path="/" component={MainSlide}/>
-                        <Route path="/register" component={Register}/>
-                        <Route path="/control" component={LoginControls}/>
-                        <Route path="/login" component={Login}/>
-                        <Route path="/logout" component={LogoutButton}/>
-                        <Route path="/library" component={BookList}/>
-                        <Route path="/rentbook" component={RentBookList}/>
-                        <Route path="/detail" component={CustomerDetail}/>
-                        <Route path="/notice" component={NoticeList}/>
-                        <Route path="/noticeadd" component={NoticeAdd}/>
-                        <Route path="/free" component={FanList}/>
-                        <Route path="/freeadd" component={FanAdd}/>
-                        <Route path="/studyadd" component={StudyAdd}/>
-                        <Route path="/study" component={StudyList}/>
-                    </Switch>
-                </div>
-            </Router>
-        </>
-    )
+class App extends Component {
+    render() {
+        return (
+                <BrowserRouter>
+                        <Switch>
+                            <Route exact path="/" component={MainSlide}/>
+                            <Route path="/control" component={LoginControl}/>
+                            <Route path="/login" component={Login}/>
+                            <Route path="/logout" component={Logout}/>
+                            <Route path="/register" component={Register}/>
+                            <Route path="/mypage" component={MyPage}/>
+                            <Route path="/library" component={BookList}/>
+                            <Route path="/rentbook" component={RentBookList}/>
+                            <Route path="/detail" component={CustomerDetail}/>
+                            <Route path="/notice" component={NoticeList}/>
+                            <Route path="/noticeadd" component={NoticeAdd}/>
+                            <Route path="/free" component={FanList}/>
+                            <Route path="/freeadd" component={FanAdd}/>
+                            <Route path="/studyadd" component={StudyAdd}/>
+                            <Route path="/study" component={StudyList}/>
+                        </Switch>
+                </BrowserRouter>
+        );
+    }
 }
 
-export default App;
+export default withCookies(App);
