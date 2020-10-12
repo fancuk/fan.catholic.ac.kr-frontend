6f4fb31fda13b452eedb4ff8a895c3e2627646ac
hyo814 <ggamjige8888@naver.com>
2020-10-13 00:32:37 +0900

    App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MainSlide from "./components/views/Home/MainSlide";
 import MyPage from "./components/views/Profile/MyPage";
-import MyBook from "./components/views/MyBook/MyBook";
 import Register from "./components/views/Login/Register";
 import Login from "./components/views/Login/Login";
 import LoginControl from "./components/views/Login/LoginControl";
@@ -14,6 +13,8 @@ import NoticeAdd from "./components/views/Notice/NoticeAdd";
 import NoticeList from "./components/views/Notice/NoticeList";
 import StudyAdd from "./components/views/Study/StudyAdd";
 import StudyList from "./components/views/Study/StudyList";
+import FreeAdd from "./components/views/Free/FreeAdd";
+import FreeList from "./components/views/Free/FreeList";
 import ScrollToTop from "./components/ScrollToTop";
 import './App.css';
 import AdminList from "./components/views/AdminBoard/AdminList";
@@ -27,7 +28,6 @@ const App = () => {
                     <ScrollToTop>
                         <Switch>
                             <Route path="/mypage" component={MyPage}/>
-                            <Route path="/mybook" component={MyBook}/>
                             <Route exact path="/" component={MainSlide}/>
                             <Route path="/register" component={Register}/>
                             <Route path="/control" component={LoginControl}/>
@@ -40,6 +40,8 @@ const App = () => {
                             <Route path="/noticeadd" component={NoticeAdd}/>
                             <Route path="/studyadd" component={StudyAdd}/>
                             <Route path="/study" component={StudyList}/>
+                            <Route path="/freeadd" component={FreeAdd}/>
+                            <Route path="/free" component={FreeList}/>
                             <Route path="/adminadd" component={AdminAdd}/>
                             <Route path="/admin" component={AdminList}/>
                         </Switch>
