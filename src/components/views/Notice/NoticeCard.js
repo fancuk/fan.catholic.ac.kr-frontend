be6f9894f0797b3c4df876f56422712c532c1d5c
hyo814 <ggamjige8888@naver.com>
2020-11-10 10:14:32 +0900

    fan.catholic.ac.kr-frontend-91 게시판 리스트

--- src/components/views/Notice/NoticeCard.js
+++ src/components/views/Notice/NoticeCard.js
@@ -1,38 +1,18 @@
 import React from "react";
-import cookie from "react-cookies";
 
-const tablespace = {
-    lineHeight: "165px"
+function NoticeCard({ title, writer, date }) {
+    return (
+        <tr>
+            <td>
+                {title}
+            </td>
+            <td>
+                {writer}
+            </td>
+            <td>
+                {date}
+            </td>
+        </tr>
+    );
 }
-
-class NoticeCard extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            user_id: cookie.load('user_id'),
-            token: cookie.load("token")
-        }
-    }
-
-    render() {
-        return (
-            <tr>
-                <td style={tablespace}>
-                    {this.props.board_name}
-                </td>
-
-                <td style={tablespace}>
-                    {this.props.title}
-                </td>
-                <td style={tablespace}>
-                    {this.props.writer}
-                </td>
-                <td style={tablespace}>
-                    {this.props.content}
-                </td>
-            </tr>
-        );
-    }
-}
-
 export default NoticeCard;
--- src/components/views/Notice/NoticeDelete.js
+++ src/components/views/Notice/NoticeDelete.js
@@ -1,68 +1,82 @@
-import React, {BaseSyntheticEvent as e} from 'react';
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import axios from "axios";
-
-class NoticeDelete extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            board_name:'',
-            title:'',
-            writer:'',
-            open: false
-        }
-    }
-    deleteNotice = () => {
-        let url = 'http://fan.catholic.ac.kr:5000/api/board/delete?title='+this.props.title;
-        axios.delete(url)
-            .then(response => {
-                console.log('response : ', JSON.stringify(response))
-            })
-            .catch(e => {
-                console.log(e);
-            })
-        this.setState({
-            open: false
-        })
-    }
-    handleClickOpen = () => {
-        this.setState({
-            open: true
-        });
-    }
-
-    handleClose = () => {
-        this.setState({
-            open: false
-        })
-    }
-    render() {
-        const {Users} = this.props;
-        return (
-            <span>
-                <Button variant="outlined" color="secondary" onClick={this.handleClickOpen}>삭제</Button>
-                <Dialog open={this.state.open} onClose={this.handleClose}>
-                    <DialogTitle onClose={this.handleClose}>
-                        삭제 경고
-                    </DialogTitle>
-                    <DialogContent>
-                        <Typography gutterBottom>
-                            선택한 게시물이 삭제됩니다.
-                        </Typography>
-                    </DialogContent>
-                    <DialogActions>
-                        <Button variant="contained" color="primary" onClick={(e) => {this.deleteNotice(this.props.title)}}>삭제</Button>
-                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
-                    </DialogActions>
-                </Dialog>
-            </span>
-        );
-    }
-};
-
-export default NoticeDelete;
+// import React, {BaseSyntheticEvent as e} from 'react';
+// import Dialog from "@material-ui/core/Dialog";
+// import DialogActions from "@material-ui/core/DialogActions";
+// import DialogTitle from "@material-ui/core/DialogTitle";
+// import DialogContent from "@material-ui/core/DialogContent";
+// import Button from "@material-ui/core/Button";
+// import Typography from "@material-ui/core/Typography";
+// import axios from "axios";
+// import cookie from "react-cookies";
+//
+// class NoticeDelete extends React.Component{
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             board_name:'noticeBoard',
+//             title:this.props.title,
+//             writer:cookie.load('user_id'),
+//             open: false,
+//             token:cookie.load('token')
+//         }
+//     }
+//     deleteStudy () {
+//         let url = 'http://fan.catholic.ac.kr:5000/api/post/delete'
+//         const notice = {
+//             board_name: 'noticeBoard',
+//             title: this.props.title,
+//             writer: cookie.load('user_id'),
+//             open: false,
+//             token: cookie.load('token')
+//         }
+//         axios.delete(url, {headers: {Authorization: ` ${cookie.load('token')}`}})
+//             .then(response => {
+//                 if (response.data.delete) {
+//                     window.location.href = '/study'
+//                 } else {
+//                     console.log('response : ', JSON.stringify(response));
+//                     alert("다시 확인 하세요")
+//                 }
+//             })
+//             .catch(e => {
+//                 console.log(e);
+//             })
+//         this.setState({
+//             delete: false
+//         })
+//     }
+//
+//     handleClickOpen = () => {
+//         this.setState({
+//             open: true
+//         });
+//     }
+//
+//     handleClose = () => {
+//         this.setState({
+//             open: false
+//         })
+//     }
+//     render() {
+//         return (
+//             <span>
+//                 <Button variant="outlined" color="secondary" onClick={this.handleClickOpen}>삭제</Button>
+//                 <Dialog open={this.state.open} onClose={this.handleClose}>
+//                     <DialogTitle onClose={this.handleClose}>
+//                         삭제 경고
+//                     </DialogTitle>
+//                     <DialogContent>
+//                         <Typography gutterBottom>
+//                             선택한 게시물이 삭제됩니다.
+//                         </Typography>
+//                     </DialogContent>
+//                     <DialogActions>
+//                         <Button variant="contained" color="primary" onClick={(e) => {this.deleteNotice()}}>삭제</Button>
+//                         <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+//                     </DialogActions>
+//                 </Dialog>
+//             </span>
+//         );
+//     }
+// };
+//
+// export default NoticeDelete;
